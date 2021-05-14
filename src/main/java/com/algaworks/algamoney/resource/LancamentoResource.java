6d07e67e5166c216bde70f09e28d98650af590e2
Heliton <Heliton@10.0.1.103>
2021-05-14 18:56:24 -0400

    Unidade 7 concluída

--- src/main/java/com/algaworks/algamoney/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoney/resource/LancamentoResource.java
@@ -30,44 +30,42 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
 
 @RestController
 @RequestMapping("/lancamentos")
 public class LancamentoResource {
-    
+
     @Autowired
     private LancamentoRepository lancamentoRepository;
-    
+
     @Autowired
     private ApplicationEventPublisher publisher;
-    
+
     @Autowired
-    private LancamentoService lacamentoService;
-    
+    private LancamentoService lancamentoService;
+
     @Autowired
     private MessageSource messageSource;
-    
+
     //--------------------------------------------------------------------------
-    
 //        @GetMapping
 //        public List<Lancamento> listar(){
 //            return lancamentoRepository.findAll();
 //        }
-    
     @GetMapping
     @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')")
-    public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable){
+    public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         return lancamentoRepository.filtrar(lancamentoFilter, pageable);
     }
-    
+
     @GetMapping(params = "resumo")
     @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')")
-    public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable){
+    public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
         return lancamentoRepository.resumir(lancamentoFilter, pageable);
     }
-    
+
     @GetMapping("/{codigo}")
     @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')")
     public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
@@ -76,30 +74,41 @@ public class LancamentoResource {
 
         return lancamento.isPresent() ? ResponseEntity.ok(lancamento.get()) : ResponseEntity.notFound().build();
     }
-    
+
     @PostMapping
     @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO')")
     public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
-        Lancamento lancamentoSalvo = lacamentoService.salvar(lancamento);
+        Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
 
         publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
     }
-    
+
     @DeleteMapping("/{codigo}")
     @PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void remover(@PathVariable Long codigo){
+    public void remover(@PathVariable Long codigo) {
         lancamentoRepository.deleteById(codigo);
     }
-    
+
     @ExceptionHandler({PessoaInexistenteOuInativaException.class})
-    public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex){
+    public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
         String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
         String mensagemDesenvolvedor = ex.toString();
         List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 
         return ResponseEntity.badRequest().body(erros);
+    }
+
+    @PutMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO')")
+    public ResponseEntity<Lancamento> atualizar(@PathVariable Long codigo, @Valid @RequestBody Lancamento lancamento) {
+        try {
+            Lancamento lancamentoSalvo = lancamentoService.atualizar(codigo, lancamento);
+            return ResponseEntity.ok(lancamentoSalvo);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/algaworks/algamoney/service/LancamentoService.java
+++ src/main/java/com/algaworks/algamoney/service/LancamentoService.java
@@ -7,30 +7,67 @@ import com.algaworks.algamoney.service.exception.PessoaInexistenteOuInativaExcep
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.algaworks.algamoney.repository.LancamentoRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 @Service
 public class LancamentoService {
 
     @Autowired
-    private LancamentoRepository lancamentoRepositiry;
+    private LancamentoRepository lancamentoRepository;
 
     @Autowired
     private PessoaRepository pessoaRepository;
 
     //--------------------------------------------------------------------------
     public Lancamento salvar(Lancamento lancamento) {
-
         try {
             Pessoa pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo()).get();
-
             if (pessoa == null || pessoa.isInativo()) {
                 throw new PessoaInexistenteOuInativaException();
             }
         } catch (Exception e) {
             throw new PessoaInexistenteOuInativaException();
         }
+        return lancamentoRepository.save(lancamento);
+    }
 
-        return lancamentoRepositiry.save(lancamento);
+    public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+        try {
+            Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+            if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+                validarPessoa(lancamento);
+            }
+
+            BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+            return lancamentoRepository.save(lancamentoSalvo);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validarPessoa(Lancamento lancamento) {
+        Pessoa pessoa = null;
+        if (lancamento.getPessoa().getCodigo() != null) {
+            pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo()).get();
+        }
+
+        if (pessoa == null || pessoa.isInativo()) {
+            throw new PessoaInexistenteOuInativaException();
+        }
+    }
+
+    private Lancamento buscarLancamentoExistente(Long codigo) {
+        try {
+            Lancamento lancamentoSalvo = lancamentoRepository.findById(codigo).get();
+            if (lancamentoSalvo == null) {
+                throw new IllegalArgumentException();
+            }
+            return lancamentoSalvo;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 
 }
